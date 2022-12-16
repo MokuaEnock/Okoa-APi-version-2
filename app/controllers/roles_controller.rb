c426237ec67ccc3f26dbdf1c40c9f48683d8c7ef
Enock <enock.mokua@student.moringaschool.com>
2022-12-16 20:36:36 +0300

    add create params for role and role params

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,2 +1,14 @@
 class RolesController < ApplicationController
+  def create
+    order = Order.create(role_params)
+    render json: order
+  end
+
+  
+
+  private
+
+  def role_params
+    params.permit(:name, :description)
+  end
 end
--- db/migrate/20221215014457_create_wastes.rb
+++ db/migrate/20221215014457_create_wastes.rb
@@ -1,6 +1,7 @@
 class CreateWastes < ActiveRecord::Migration[7.0]
   def change
     create_table :wastes do |t|
+      
 
       t.timestamps
     end
