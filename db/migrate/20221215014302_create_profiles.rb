5fa3d24970db260b9cf1765ff1e721601c9874f5
Enock <enock.mokua@student.moringaschool.com>
2022-12-16 20:30:03 +0300

    create profile attributes

--- db/migrate/20221215014302_create_profiles.rb
+++ db/migrate/20221215014302_create_profiles.rb
@@ -1,6 +1,26 @@
 class CreateProfiles < ActiveRecord::Migration[7.0]
   def change
     create_table :profiles do |t|
+      t.belongs_to :user, foreign_key: true
+      t.string :name
+      t.string :gender
+      t.string :disability
+      t.string :disability_type
+      t.string :gender
+      t.string :marital_status
+      t.string :living_with
+      t.string :head_household
+      t.string :sector_working
+      t.string :individuals_living
+      t.string :date_of_birth
+      t.string :email
+      t.string :county
+      t.string :sub_county
+      t.string :location
+      t.string :marital_status
+      t.string :occupation
+      t.string :phone_number
+      t.string :id_number
 
       t.timestamps
     end
