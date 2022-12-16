46fee2eed683a8504c62ce172284ff080981d96f
Enock <enock.mokua@student.moringaschool.com>
2022-12-16 21:05:00 +0300

    add only routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :disposals
   resources :wastes
   resources :profiles
-  resources :roles
+  resources :roles, only: %i[create show update destroy]
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
