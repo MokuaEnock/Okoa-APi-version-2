bb62dfb435a87c26f7548d5fbb9b4c917901c7e5
Enock <enock.mokua@student.moringaschool.com>
2022-12-17 05:53:29 +0300

    add create user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
-  
   def create
+    user = User.create(user_params)
+    render json: user
   end
 
   private
 
   def user_params
-    params.permit(:username, :email, :password_digest)
+    params.permit(:username, :email, :password, :password_confirmation)
   end
 end
