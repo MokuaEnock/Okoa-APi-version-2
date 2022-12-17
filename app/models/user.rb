cf5513048d5c11fa12e9b655c2414103fb8c76be
Enock <enock.mokua@student.moringaschool.com>
2022-12-17 07:08:48 +0300

    add validators for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :username,
+            presence: true,
+            uniqueness: true,
+            length: {
+              minimum: 4,
+              maximum: 16
+            }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8, maximum: 16 }
 end
