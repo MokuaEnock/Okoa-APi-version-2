0d21bdd8cada3738f35fb0d28a97171f423d3c91
Enock <enock.mokua@student.moringaschool.com>
2022-12-18 08:03:07 +0300

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Role.create do
+  [
+    {
+      name: "Disposer",
+      description:
+        "Users who are responsible for disposing off waste and creating alerts which triggers waste collectors to be disposed."
+    },
+    {
+      name: "Collector",
+      description:
+        "Users responsible for collecting waste weighing it sorting it and delivering it to the site."
+    },
+    {
+      name: "Partners",
+      description:
+        "Users from organizations that are responsible for disposing and collecting inorganic waste."
+    },
+    {
+      name: "Admin",
+      description:
+        "A superuser who can create, read, update and delete anything."
+    },
+    { name: "Buyer", description: "A user who can be able to buy compost" }
+  ]
+end
