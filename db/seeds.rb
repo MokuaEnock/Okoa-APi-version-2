d27ce4aa18506d8ef84913b0e0c030e2134c1200
Enock <enock.mokua@student.moringaschool.com>
2022-12-18 08:22:32 +0300

    reformat seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,51 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Role.create do
-  [
-    {
-      name: "Disposer",
-      description:
-        "Users who are responsible for disposing off waste and creating alerts which triggers waste collectors to be disposed."
-    },
-    {
-      name: "Collector",
+# Role.create do
+#   [
+#     {
+#       name: "Disposer",
+#       description:
+#         "Users who are responsible for disposing off waste and creating alerts which triggers waste collectors to be disposed."
+#     },
+#     {
+#       name: "Collector",
+#       description:
+#         "Users responsible for collecting waste weighing it sorting it and delivering it to the site."
+#     },
+#     {
+#       name: "Partners",
+#       description:
+#         "Users from organizations that are responsible for disposing and collecting inorganic waste."
+#     },
+#     {
+#       name: "Admin",
+#       description:
+#         "A superuser who can create, read, update and delete anything."
+#     },
+#     { name: "Buyer", description: "A user who can be able to buy compost" }
+#   ]
+# end
+
+Role.create(
+  name: "Disposer",
+  description:
+    "Users who are responsible for disposing off waste and creating alerts which triggers waste collectors to be disposed."
+)
+
+Role.create(
+ name: "Collector",
       description:
         "Users responsible for collecting waste weighing it sorting it and delivering it to the site."
-    },
-    {
-      name: "Partners",
-      description:
-        "Users from organizations that are responsible for disposing and collecting inorganic waste."
-    },
-    {
-      name: "Admin",
+)
+
+
+Role.create(
+  name: "Admin",
       description:
         "A superuser who can create, read, update and delete anything."
-    },
-    { name: "Buyer", description: "A user who can be able to buy compost" }
-  ]
-end
+)
+
+Role.create(
+  name: "Buyer", description: "A user who can be able to buy compost"
+)
