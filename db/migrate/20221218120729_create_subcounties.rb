9bda3b136d1c21a53267c52fb15622f23e4aed4f
Enock <enock.mokua@student.moringaschool.com>
2022-12-18 15:10:30 +0300

    add attributes to counties subcounties and divisions

--- db/migrate/20221218120729_create_subcounties.rb
+++ db/migrate/20221218120729_create_subcounties.rb
@@ -1,6 +1,8 @@
 class CreateSubcounties < ActiveRecord::Migration[7.0]
   def change
     create_table :subcounties do |t|
+      t.belongs_to :county
+      t.string :name
 
       t.timestamps
     end
--- db/migrate/20221218120738_create_divisions.rb
+++ db/migrate/20221218120738_create_divisions.rb
@@ -1,6 +1,8 @@
 class CreateDivisions < ActiveRecord::Migration[7.0]
   def change
     create_table :divisions do |t|
+      t.belongs_to :subcounties
+      t.string :name
 
       t.timestamps
     end
